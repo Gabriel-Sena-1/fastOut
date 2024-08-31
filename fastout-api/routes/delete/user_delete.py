5f3b2c89e5e8f95ce087f87a3f5f745175004b54
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-08-31 19:31:47 -0400

    add: delete route API

---
+++ fastout-api/routes/delete/user_delete.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class Request(BaseModel):
+    image_id: int
+    correct_classification: str
+    
+@router.delete("/user/{user_id}")
+def func4()->str:
+    return "essa eh a funcao 4"
+
