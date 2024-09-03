b73fb7ad0d04ca9ea062b1b3bc7303a8d11c5bb9
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-02 23:30:03 -0400

    :recycle: refactor: alterando estrutura do banco

--- meu_bolso-api/db/meubolso_DB.sql
+++ meu_bolso-api/db/meubolso_DB.sql
@@ -1,18 +1,16 @@
--- Criação do banco de dados
-CREATE DATABASE IF NOT EXISTS controle_gastos_pessoais;
-USE controle_gastos_pessoais;
-
 -- Tabela de Usuários
 CREATE TABLE usuarios (
     id_user INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     sobrenome VARCHAR(100) NOT NULL,
     email VARCHAR(200) NOT NULL UNIQUE,
-    senha VARCHAR(100) NOT NULL
+    senha VARCHAR(100) NOT NULL,
+    tipo_usuario INT NOT NULL
 );
 
+
 -- Tabela de Grupos (Natureza do gasto)
--- cadastro previo: contas, lazer, economias
+-- cadastro previo: Alimentacao, transporte, lazer, educacao
 CREATE TABLE grupos (
     id_grupo INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL 
@@ -35,6 +33,21 @@ CREATE TABLE gasto_grupo (
     FOREIGN KEY (id_grupo) REFERENCES grupos(id_grupo) ON DELETE CASCADE
 );
 
+-- Tabela de relacionamento entre Usuarios e Grupos
+CREATE TABLE usuario_grupo (
+    id_user INT,
+    id_grupo INT,
+    PRIMARY KEY (id_usuario, id_grupo),
+    FOREIGN KEY (id_user) REFERENCES usuarios(id_user) ON DELETE CASCADE,
+    FOREIGN KEY (id_grupo) REFERENCES grupos(id_grupo) ON DELETE CASCADE
+);
+
+INSERT INTO grupo (nome) VALUES ('Alimentação');
+INSERT INTO grupo (nome) VALUES ('Transporte');
+INSERT INTO grupo (nome) VALUES ('Educação');
+INSERT INTO grupo (nome) VALUES ('Lazer');
+
 -- Índices para otimização de consultas
 CREATE INDEX idx_usuarios_email ON usuarios(email);
 CREATE INDEX idx_gastos_data ON gastos(data);
+
