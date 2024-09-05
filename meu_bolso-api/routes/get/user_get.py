3074573f95ce064378f4726a36db6d1ed35c66f4
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-05 02:39:56 -0400

    feat: route get user pronta

--- meu_bolso-api/routes/get/user_get.py
+++ meu_bolso-api/routes/get/user_get.py
@@ -1,6 +1,8 @@
-import json
+
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from ...model.User import User
 
 router = APIRouter()
 
@@ -8,14 +10,16 @@ class Request(BaseModel):
     id_user: int
 
 @router.get("/user")
-def retornaTodosUsuarios()->json:
-    usuarios = {}
-    #todo: acumula todos os usuarios em value
-    return json.dumps(usuarios) #? retorna usuarios 
+def retornaTodosUsuarios() -> JSONResponse:
+    usuarios = User.buscar_todos()
+    if not usuarios:
+        raise HTTPException(status_code=404, detail="Nenhum usuário encontrado.")
+    return JSONResponse(content=usuarios)
 
 @router.get("/user/{id_user}")
-def retornaUmUsuario()->json:
-    usuario = {}
-    #todo: acumula o usuario em value
-    return json.dumps(usuario) #? retorna o usuario
+def retornaUmUsuario(id_user: int) -> JSONResponse:
+    usuario = User.buscar_por_id(id_user=id_user)
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    return JSONResponse(content=usuario)
 
