0d94001b0ea61cd49b7328074fd74c37a069f6ef
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-05 19:19:39 -0400

    feat: basemodel para parsear json nas respostas de requisicoes get

--- meu_bolso-api/model/User.py
+++ meu_bolso-api/model/User.py
@@ -1,23 +1,112 @@
-import mysql.connector
+from datetime import datetime
+from db.database_manager import DatabaseManager
+import os
+from pydantic import BaseModel
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword",
-  database="mydatabase"
-)
+host = str(os.getenv("DB_HOST"))
+db = DatabaseManager("db_container", os.getenv("DB_USER"), os.getenv("DB_PASSWORD"), os.getenv("DB_NAME"))
 
-mycursor = mydb.cursor()
+class UserBase(BaseModel):
+    nome: str
+    sobrenome: str
+    email: str
+    senha: str
+    tipo_usuario: int
+    qtd_grupos: int
+    ativo: bool
+
+class UserResponse(UserBase):
+    id_user: int
 
 class User:
-    def __init__(self, nome, sobrenome, email, senha):
-        self.id_user = None  # Será definido pelo banco de dados
+    def __init__(self, nome, sobrenome, email, senha, tipo_usuario, qtd_grupos, ativo=True):
+        self.id_user = None
         self.nome = nome
         self.sobrenome = sobrenome
         self.email = email
         self.senha = senha
+        self.tipo_usuario = tipo_usuario
+        self.qtd_grupos = qtd_grupos
+        self.ativo = ativo
 
-    def __str__(self):
-        return f"User(id={self.id_user}, nome={self.nome}, sobrenome={self.sobrenome}, email={self.email})"
-    
-    mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+    def salvar(self):
+        db.connect()
+        if self.id_user is None:
+            sql = "INSERT INTO usuarios (nome, sobrenome, email, senha, tipo_usuario, qtd_grupos, ativo) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+            val = (self.nome, self.sobrenome, self.email, self.senha, self.tipo_usuario, self.qtd_grupos, self.ativo)
+            db.execute(sql, val)
+            db.commit()
+            self.id_user = db.lastrowid
+        else:
+            sql = "UPDATE usuarios SET nome = %s, sobrenome = %s, email = %s, senha = %s, tipo_usuario = %s, qtd_grupos = %s, ativo = %s WHERE id_user = %s"
+            val = (self.nome, self.sobrenome, self.email, self.senha, self.tipo_usuario, self.qtd_grupos, self.ativo, self.id_user)
+            db.execute(sql, val)
+            db.commit()
+        db.disconnect()
+
+    @staticmethod
+    def buscar_por_id(id_user: int) -> UserResponse:
+        db.connect()
+        sql = "SELECT * FROM usuarios WHERE id_user = %s"
+        val = (id_user,)
+        db.execute(sql, val)
+        result = db.fetchone()
+        db.disconnect()
+
+        if result:
+            user = UserResponse(
+                id_user=result[0],
+                nome=result[1],
+                sobrenome=result[2],
+                email=result[3],
+                senha=result[4],
+                tipo_usuario=result[5],
+                qtd_grupos=result[6],
+                ativo=result[7]
+            )
+            return user
+
+        return None
+
+
+    @staticmethod
+    def buscar_todos():
+        db.connect()
+        sql = "SELECT * FROM usuarios"
+        db.execute(sql)
+        results = db.fetchall()
+        db.disconnect()
+        users = []
+        for result in results:
+            user = UserResponse(
+                id_user=result[0],
+                nome=result[1],
+                sobrenome=result[2],
+                email=result[3],
+                senha=result[4],
+                tipo_usuario=result[5],
+                qtd_grupos=result[6],
+                ativo=result[7]
+            )
+            users.append(user)
+        return users
+
+
+    def atualizar(self):
+        if self.id_user is not None:
+            db.connect()
+            sql = "UPDATE usuarios SET nome = %s, sobrenome = %s, email = %s, senha = %s, tipo_usuario = %s, qtd_grupos = %s, ativo = %s WHERE id_user = %s"
+            val = (self.nome, self.sobrenome, self.email, self.senha, self.tipo_usuario, self.qtd_grupos, self.ativo, self.id_user)
+            db.execute(sql, val)
+            db.commit()
+            db.disconnect()
+
+    def deletar(self):
+        if self.id_user is not None:
+            db.connect()
+            sql = "DELETE FROM usuarios WHERE id_user = %s"
+            val = (self.id_user,)
+            db.execute(sql, val)
+            db.commit()
+            db.disconnect()
+            self.id_user = None
