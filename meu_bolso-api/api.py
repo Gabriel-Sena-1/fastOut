2e9a0ef5640b7ee45c83daa51af9f5da6af7bb68
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-06 19:00:33 -0400

    :sparkles: feat: rota de delete user completa

--- meu_bolso-api/api.py
+++ meu_bolso-api/api.py
@@ -1,12 +1,15 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
-from routes.get.user_get import router as user_router  # Importe o roteador de usuários
-from routes.get.gasto_get import router as gastos_router  # Importe o roteador de usuários
+from routes.get.user_get import router as user_router  
+from routes.delete.user_delete import router as user_delete_router  
+from routes.get.gasto_get import router as gastos_router  
+
 
 # Inicialização do FastAPI
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(user_delete_router, prefix="/users", tags=["Users"])
 app.include_router(gastos_router, prefix="/gastos", tags=["Gastos"])
 
 if __name__ == "__main__":
