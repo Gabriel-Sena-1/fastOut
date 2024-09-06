ff142272737a2e3ca7dcd7ec1ef045350eed88fb
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-06 19:02:22 -0400

    refactor: nome das rotas alterado para swagger ficar bonito

--- meu_bolso-api/routes/get/user_get.py
+++ meu_bolso-api/routes/get/user_get.py
@@ -5,14 +5,14 @@ from model.User import User
 router = APIRouter()
 
 @router.get("")
-def retornaTodosUsuarios() -> JSONResponse:
+def retorna_todos_usuarios() -> JSONResponse:
     usuarios = User.buscar_todos()
     if not usuarios:
         raise HTTPException(status_code=404, detail="Nenhum usuário encontrado.")
     return JSONResponse(content=[user.model_dump() for user in usuarios])
 
 @router.get("/{id_user}")
-def retornaUmUsuario(id_user: int) -> JSONResponse:
+def retorna_um_usuario(id_user: int) -> JSONResponse:
     usuario = User.buscar_por_id(id_user=id_user)
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuário não encontrado.")
