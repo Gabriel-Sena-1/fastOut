d12d0772625b053d8bab5f35a093ec21f57e1915
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-06 00:03:19 -0400

    :sparkles: feat: finalmente endpoint de users corrigido

--- meu_bolso-api/api.py
+++ meu_bolso-api/api.py
@@ -1,39 +1,10 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
-from database_manager import DatabaseManager
-
-# Configuração do banco de dados
-db_manager = DatabaseManager(
-    host="localhost",
-    user="db",
-    password="&b7Lq!Xy29D#Wj4N",
-    database="meubolso"
-)
-
-# Conectar ao banco de dados
-db_manager.connect()
-
-# Criação do banco de dados (se necessário)
-db_manager.execute("CREATE DATABASE IF NOT EXISTS meubolso")
-db_manager.commit()
-
-# Seleciona o banco de dados
-db_manager.execute("USE meubolso")
-
-# Lê e executa o arquivo SQL
-with open("schema.sql", "r") as file:
-    sql_script = file.read()
-
-for statement in sql_script.split(';'):
-    if statement.strip():
-        db_manager.execute(statement)
-
-# Confirma as mudanças e desconecta
-db_manager.commit()
-db_manager.disconnect()
-
+from routes.get.user_get import router as user_router  # Importe o roteador de usuários
 # Inicialização do FastAPI
 app = FastAPI()
+
+app.include_router(user_router, prefix="/users", tags=["Users"])
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
