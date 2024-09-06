6bc6a2aa640429d4db5f52883c6274cd3a13b364
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-06 17:07:40 -0400

    feat: adicionando roda de gastos na api

--- meu_bolso-api/api.py
+++ meu_bolso-api/api.py
@@ -1,10 +1,13 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from routes.get.user_get import router as user_router  # Importe o roteador de usuários
+from routes.get.gasto_get import router as gastos_router  # Importe o roteador de usuários
+
 # Inicialização do FastAPI
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(gastos_router, prefix="/gastos", tags=["Gastos"])
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
