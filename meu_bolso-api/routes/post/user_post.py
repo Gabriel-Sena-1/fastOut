ad081f7988f81ba259fd2edfd4d89441da3522f0
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-10 00:20:09 -0400

    feat: user post pronto

--- meu_bolso-api/routes/post/user_post.py
+++ meu_bolso-api/routes/post/user_post.py
@@ -1,16 +1,30 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
+from model.User import User
 from pydantic import BaseModel
 
 router = APIRouter()
 
-class Request(BaseModel):
-    image_id: int
-    correct_classification: str
+# Definindo o modelo Pydantic que será recebido no corpo da requisição
+class UserCreate(BaseModel):
+    nome: str
+    sobrenome: str
+    email: str
+    senha: str
 
-@router.post("/login")
-def func1()->str:
-    return "essa eh a funcao 1"
+# Rota POST que recebe os dados via corpo da requisição
+@router.post("")
+def criar_usuario(user: UserCreate):
+    # Aqui você pode processar os dados recebidos
+    usuario = User(
+        nome=user.nome,
+        sobrenome=user.sobrenome,
+        email=user.email,
+        senha=user.senha
+    )
+    # Salvar o usuário no banco de dados
+    cria_usuario = usuario.salvar()
 
-@router.post("/user")
-def func2()->str:
-    return "essa eh a funcao 1"
+    if not cria_usuario:
+        raise HTTPException(status_code=404, detail="Falha ao criar o usuário.")
+    return JSONResponse(content={"message": "Sucesso ao cadastrar o usuário!"})
