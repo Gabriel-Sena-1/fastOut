96690ff5e115736ddcaaa435e604938cdebd47ba
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-10 00:21:21 -0400

    feat: estrutura arquivo api feita

--- meu_bolso-api/api.py
+++ meu_bolso-api/api.py
@@ -1,16 +1,38 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
-from routes.get.user_get import router as user_router  
-from routes.delete.user_delete import router as user_delete_router  
-from routes.get.gasto_get import router as gastos_router  
 
+from fastapi import FastAPI
+from routes.post.user_post import router as user_post_router            # C
+from routes.get.user_get import router as user_get_router               # R
+from routes.put.user_put import router as user_put_router               # U
+from routes.delete.user_delete import router as user_delete_router      # D
+
+from routes.post.gasto_post import router as gastos_post_router         # C
+from routes.get.gasto_get import router as gastos_get_router            # R
+from routes.put.gasto_put import router as gastos_put_router            # U
+from routes.delete.gasto_delete import router as gastos_delete_router   # D
+
+from routes.post.grupo_post import router as grupos_post_router         # C
+from routes.get.grupo_get import router as grupos_get_router            # R
+from routes.put.grupo_put import router as grupos_put_router            # U
+from routes.delete.grupo_delete import router as grupos_delete_router   # D
 
 # Inicialização do FastAPI
 app = FastAPI()
 
-app.include_router(user_router, prefix="/users", tags=["Users"])
+app.include_router(user_post_router, prefix="/users", tags=["Users"])
+app.include_router(user_get_router, prefix="/users", tags=["Users"])
+app.include_router(user_put_router, prefix="/users", tags=["Users"])
 app.include_router(user_delete_router, prefix="/users", tags=["Users"])
-app.include_router(gastos_router, prefix="/gastos", tags=["Gastos"])
+
+app.include_router(gastos_post_router, prefix="/gastos", tags=["Gastos"]) 
+app.include_router(gastos_get_router, prefix="/gastos", tags=["Gastos"]) 
+app.include_router(gastos_put_router, prefix="/gastos", tags=["Gastos"]) 
+app.include_router(gastos_delete_router, prefix="/gastos", tags=["Gastos"]) 
+
+app.include_router(grupos_post_router, prefix="/grupos", tags=["Grupos"]) 
+app.include_router(grupos_get_router, prefix="/grupos", tags=["Grupos"]) 
+app.include_router(grupos_put_router, prefix="/grupos", tags=["Grupos"]) 
+app.include_router(grupos_delete_router, prefix="/grupos", tags=["Grupos"]) 
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
