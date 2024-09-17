992478cbaa2154f9fea15589bdb2a54024c37a0c
Gabriel-Sena-1 <gabrielmirandasena11@gmail.com>
2024-09-17 11:24:11 -0400

    feat: criar grupo  com validacao da qtd de grupos do usuario

--- meu_bolso-api/routes/post/grupo_post.py
+++ meu_bolso-api/routes/post/grupo_post.py
@@ -1,14 +1,29 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+from model.Grupo import Grupo
+from fastapi.responses import JSONResponse
 
 router = APIRouter()
 
-class Request(BaseModel):
-    image_id: int
-    correct_classification: str
+class GrupoCreate(BaseModel):
+    nome: str
+    id_user: int
 
-@router.post("/grupo")
-def func1()->str:
-    return "essa eh a funcao 1"
+@router.post("")
+def criar_grupo(grupo_data: GrupoCreate) -> JSONResponse:
+    try:
+        # Criando o objeto Grupo com o nome fornecido
+        grupo = Grupo(nome=grupo_data.nome)
 
-
+        # Salvando o grupo no banco de dados e associando ao usuário
+        sucesso = grupo.salvar(id_user=grupo_data.id_user)
+        
+        if not sucesso:
+            raise HTTPException(status_code=400, detail="Falha ao criar o grupo.")
+        
+        return JSONResponse(content={"message": "Grupo criado e associado com sucesso!"})
+    
+    except Exception as e:
+        # Captura qualquer exceção e gera uma resposta de erro
+        print(f"Erro ao criar o grupo: {e}")
+        raise HTTPException(status_code=500, detail="Erro interno do servidor")
